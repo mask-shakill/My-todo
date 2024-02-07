f94c816136d6218226eb0839a383012ac6dfedb7
mask-shakill <mask-shakill>
2024-02-07 19:47:05 +0600

    complete task create, delete, update task added

---
+++ src/components/Create-Single-Task/CreateTask.jsx
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+const CreateTask = () => {
+  const generateUniqueId = () => {
+    return Date.now().toString(36) + Math.random().toString(36).slice(2, 11);
+  };
+
+  const [formData, setFormData] = useState({
+    id: generateUniqueId(),
+    title: "",
+    description: "",
+    priority: "",
+    status: "Incomplete",
+    deadline: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTask = {
+      ...formData,
+      id: generateUniqueId(),
+    };
+
+    const exData = JSON.parse(localStorage.getItem("taskData")) || [];
+    const updateData = [...exData, newTask];
+    localStorage.setItem("taskData", JSON.stringify(updateData));
+    navigate("/");
+  };
+
+  return (
+    <div className="mt-10">
+      <Link
+        to={"/"}
+        className="text-2xl bg-slate-600 px-5 py-1 rounded text-white"
+      >
+        Back
+      </Link>
+      <section className="flex flex-col  items-center md:flex-col md:items-center">
+        <h1 className="text-3xl font-semibold mt-10 mb-3">Create task</h1>
+        <form className="flex flex-col gap-y-3 bg-gray-300 w-[100%] md:w-[60%] items-center py-10 rounded-lg">
+          <input
+            id="title"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            className="md:w-1/2 h-14 rounded-md p-2 text-lg"
+            placeholder="Enter Title"
+            type="text"
+          />
+          <input
+            name="description"
+            value={formData.description}
+            onChange={handleChange}
+            className="md:w-1/2 h-14 rounded-md p-2 text-lg"
+            placeholder="Enter description"
+            type="text"
+          />
+          <select
+            name="priority"
+            value={formData.priority}
+            onChange={handleChange}
+            className="w-[85%] md:w-1/2 h-14 rounded-md p-2"
+          >
+            <option value="" disabled defaultValue>
+              Select Priority
+            </option>
+            <option value="High">High</option>
+            <option value="Medium">Medium</option>
+            <option value="Low">Low</option>
+          </select>
+          <input
+            className="w-[85%] md:w-1/2 rounded h-14 p-2"
+            type="date"
+            name="deadline"
+            onChange={handleChange}
+            value={formData.deadline}
+            id=""
+          />
+          <button
+            onClick={handleSubmit}
+            className="w-1/2 h-14 rounded-md bg-blue-500 hover:bg-blue-700 text-white font-semibold"
+          >
+            Submit
+          </button>
+        </form>
+      </section>
+    </div>
+  );
+};
+
+export default CreateTask;
