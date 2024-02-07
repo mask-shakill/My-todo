ce4ebe469d995388fbd232481de2c13f9b431325
mask-shakill <mask-shakill>
2024-02-07 17:45:12 +0600

    Header added

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { FaRegEdit } from "react-icons/fa";
+import { Link } from "react-router-dom";
+const Header = () => {
+  return (
+    <main className="">
+      <section className="flex lg:flex justify-between items-center p-2 lg:items-center  lg:justify-between lg:p-5">
+        <div className="text-white flex gap-x-4 items-center   lg:flex lg:items-center lg:gap-x-3">
+          <img
+            className="w-[35px] lg:w-[70px] rounded-full"
+            src="https://scontent.fdac14-1.fna.fbcdn.net/v/t39.30808-6/364804848_791739752953899_5445063310506889506_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=efb6e6&_nc_eui2=AeHJw3nwgKtKERI5bxw18RdN0DUfkZvfNsjQNR-Rm982yECPUxEqmVqRI7m_XHLH-l0R2auXkC2ZCDi74jZmZm2Z&_nc_ohc=H4iN6AM_EZAAX9K9QMT&_nc_ht=scontent.fdac14-1.fna&oh=00_AfDKqbbrH-hQuAG_yOpFtQjWX8VE5Wcc_4MVzGRJPBMVEw&oe=65C7DD68"
+            alt=""
+          />
+          <div>
+            <Link
+              to={"/"}
+              className=" text-lg lg:text-5xl font-semibold text-blue-900"
+            >
+              Qtec to-do
+            </Link>
+          </div>
+        </div>
+
+        {/* Task create button start here  */}
+        <Link
+          to={"/task-create"}
+          className=" flex items-center gap-x-2  text-blue-900 text-sm font-semibold lg:text-3xl lg:font-bold  rounded-lg px-2"
+        >
+          Create Task <FaRegEdit />
+        </Link>
+        {/* Button end here  */}
+      </section>
+      <hr className="h-1 bg-blue-900" />
+    </main>
+  );
+};
+
+export default Header;
